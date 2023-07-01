7f3b4006012709002a59a794a4724b0a09bb5119
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-07-01 13:30:39 +0530

    new post and logout button in phone added

--- src/components/PhonePanel/PanelTop.jsx
+++ src/components/PhonePanel/PanelTop.jsx
@@ -1,10 +1,32 @@
 import "./PanelTop.css";
 import { icon } from "../../img";
 import SearchBar from "../SearchBar/SearchBar";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../context/AuthContext";
 
-export default function PanelTop(){
-    return <div className="panel-top">
-        <img src={icon} alt="icon" width="70" height="70"/>
-        <SearchBar />
+export default function PanelTop() {
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAuth();
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+        localStorage.removeItem("encodedToken");
+      }
+
+  return (
+    <div className="panel-top">
+      <img src={icon} alt="icon" width="50" height="50" />
+      <SearchBar />
+      <div className="phone-icons">
+        <img
+          width="30"
+          height="30"
+          src="https://img.icons8.com/external-regular-kawalan-studio/96/external-new-post-social-media-regular-kawalan-studio-2.png"
+          alt="external-new-post-social-media-regular-kawalan-studio-2"
+          onClick={()=>navigate("/new-post")}
+        />
+        <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/90/exit.png" alt="exit" onClick={logoutHandler}/>
+      </div>
     </div>
+  );
 }
