9a214a68f575b21a67298b0685492a26638237c2
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-07-02 11:57:48 +0530

    route fix

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,28 +7,28 @@ import PostPage from "../Pages/Private/PostPage/PostPage";
 import Mockman from "mockman-js";
 import EditPost from "../Pages/Private/PostPage/EditPost";
 import NewPost from "../Pages/Private/PostPage/NewPost";
-import AuthRoute from "../components/AuthRoutes/AuthRoute";
+// import AuthRoute from "../components/AuthRoutes/AuthRoute";
 
 export default function RoutePages(){
-    return <Routes>
-        <Route path="/" element={<AuthRoute><HomePage /></AuthRoute>} />
-        <Route path="/explore" element={<AuthRoute><ExplorePage /></AuthRoute>} />
-        <Route path="/bookmark" element={<AuthRoute><BookmarkPage /></AuthRoute>} />
-        <Route path="/profile/:userId" element={<AuthRoute><ProfilePage /></AuthRoute>} />
-        <Route path="/post/:postId" element={<AuthRoute><PostPage /></AuthRoute>}/>
-        <Route path='/mockman' element={<Mockman />} />
-        <Route path="/edit/:postId" element={<AuthRoute><EditPost/></AuthRoute>} />
-        <Route path="/new-post" element={<AuthRoute><NewPost /></AuthRoute>}/>
-    </Routes>
     // return <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //     <Route path="*" element={<HomePage />} />
-    //     <Route path="/explore" element={<ExplorePage />} />
-    //     <Route path="/bookmark" element={<BookmarkPage />} />
-    //     <Route path="/profile/:userId" element={<ProfilePage />} />
-    //     <Route path="/post/:postId" element={<PostPage />}/>
+    //     <Route path="/" element={<AuthRoute><HomePage /></AuthRoute>} />
+    //     <Route path="/explore" element={<AuthRoute><ExplorePage /></AuthRoute>} />
+    //     <Route path="/bookmark" element={<AuthRoute><BookmarkPage /></AuthRoute>} />
+    //     <Route path="/profile/:userId" element={<AuthRoute><ProfilePage /></AuthRoute>} />
+    //     <Route path="/post/:postId" element={<AuthRoute><PostPage /></AuthRoute>}/>
     //     <Route path='/mockman' element={<Mockman />} />
-    //     <Route path="/edit/:postId" element={<EditPost/>} />
-    //     <Route path="/new-post" element={<NewPost />}/>
+    //     <Route path="/edit/:postId" element={<AuthRoute><EditPost/></AuthRoute>} />
+    //     <Route path="/new-post" element={<AuthRoute><NewPost /></AuthRoute>}/>
     // </Routes>
+    return <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/bookmark" element={<BookmarkPage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path="/post/:postId" element={<PostPage />}/>
+        <Route path='/mockman' element={<Mockman />} />
+        <Route path="/edit/:postId" element={<EditPost/>} />
+        <Route path="/new-post" element={<NewPost />}/>
+    </Routes>
 }
